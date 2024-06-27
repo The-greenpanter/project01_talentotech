3eaf20f29ddd3debbce536c8c857a6ca8d1be7ce
The-greenpanter <judpenaca@unal.edu.co>
2024-06-27 07:58:13 -0500

    FirstComponent

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import MyFirstComponent from './components/MyFirstComponent';
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          <MyFirstComponent/>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
